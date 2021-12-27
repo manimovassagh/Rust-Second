5cb80290e3788afb08abbfc7d836db8bad5aaef0
Mani Movassagh <mani.mgh@gmail.com>
2021-12-27 17:48:18 +0100

    match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use core::panicking::AssertKind::Match;
+
+#[warn(non_snake_case)]
+
 fn main() {
     println!("Hello, world!");
     println!("Hello, Mani!");
@@ -6,6 +10,18 @@ fn main() {
 
 fn for_loop(){
     for x in 1..12{
-        println!("x is equal to {}", x)
+        println!("x is equal to {}", x);
+        print!("some\n other")
     }
+}
+
+fn match_coder(){
+    let country_code=98;
+    let country=match country_code {
+        98=>"Iran",
+        44=>"UK",
+        01=>"USA",
+        1..=1000 => "unknown",
+        _ => "invalid",
+    };
 }
