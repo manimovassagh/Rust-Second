54da976a2b6cc4125092eba544d71ba00ac10f0a
Mani Movassagh <mani.mgh@gmail.com>
2021-12-27 17:37:14 +0100

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     println!("Hello, world!");
     println!("Hello, Mani!");
+    for_loop();
+}
+
+fn for_loop(){
+    for x in 1..12{
+        println!("x is equal to {}", x)
+    }
 }
