9c45b4ae5e4562d655fe5036450ada8502fa56b7
Mani Movassagh <mani.mgh@gmail.com>
2021-12-29 13:53:15 +0100

    emoji in Ruat

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod strings;
 use std::net::TcpListener;
 
 #[warn(non_snake_case)]
-
 fn main() {
     println!("Hello, world!");
     println!("Hello, Mani!");
@@ -12,7 +11,8 @@ fn main() {
     match_coder();
     match_coder();
     match_coder();
-some::run();
+    some::run();
+    strings::run();
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
@@ -20,25 +20,23 @@ some::run();
 
         println!("Connection established!");
     }
-
 }
 
-fn for_loop(){
-    for x in 1..12{
+fn for_loop() {
+    for x in 1..12 {
         println!("x is equal to {}", x);
-
     }
 }
 
-fn match_coder(){
-    let country_code=98;
-    let country=match country_code {
-        98=>"Iran",
-        44=>"UK",
-        01=>"USA",
+fn match_coder() {
+    let country_code = 98;
+    let country = match country_code {
+        98 => "Iran",
+        44 => "UK",
+        01 => "USA",
         1..=1000 => "unknown",
         _ => "invalid",
     };
 
-    println!("The country name with country code {} is {}", country_code,country)
+    println!("The country name with country code {} is {}", country_code, country)
 }
