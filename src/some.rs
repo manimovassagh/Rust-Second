5ad00a7013c96028b005d034ce939064eeb1f659
Mani Movassagh <mani.mgh@gmail.com>
2021-12-29 13:50:14 +0100

    emoji in Ruat

--- src/some.rs
+++ src/some.rs
@@ -1,4 +1,6 @@
 pub(crate) fn run (){
     let name ="mani";
-    println!("This is my name : {}", name)
+    println!("This is my name : {}", name);
+    let face = '\u{1F600}';
+    println!("this is a smile from Rust {}",face);
 }
