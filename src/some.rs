6260969a0767547e74bbb268f825c1a0cff3d22b
Mani Movassagh <mani.mgh@gmail.com>
2021-12-29 14:02:55 +0100

    module

--- src/some.rs
+++ src/some.rs
@@ -1,6 +1,9 @@
+
+
 pub(crate) fn run (){
     let name ="mani";
     println!("This is my name : {}", name);
     let face = '\u{1F600}';
     println!("this is a smile from Rust {}",face);
+    let string_sample =String::from("Mani Movassagh");
 }
