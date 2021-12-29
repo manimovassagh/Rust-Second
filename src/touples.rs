96fea4fb97ccdc1d5a1263104bb83a7dc6f9a6b2
Mani Movassagh <mani.mgh@gmail.com>
2021-12-29 14:56:35 +0100

    touple

--- src/touples.rs
+++ src/touples.rs
@@ -1,3 +1,5 @@
 pub fn run(){
+let persons: (&str, &str, &mut i32) =("Mehdi", "Rahmaniyan", &mut 33);
+    println!("My name is {} and my family is {} and i am {} years old",persons.0,persons.1,persons.2);
 
 }
