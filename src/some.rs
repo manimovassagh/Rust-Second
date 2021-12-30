83c926b1a5ad9b922e83c4cdb426cbfed91683b4
Mani Movassagh <mani.mgh@gmail.com>
2021-12-30 03:02:18 +0100

    mm

--- src/some.rs
+++ src/some.rs
@@ -9,7 +9,7 @@ pub(crate) fn run (){
     println!("{}",string_sample);
     string_sample.push_str("Some More");
     println!("{}",string_sample);
-       println!("Length of The String is {}",string_sample.len());
+    println!("Length of The String is {}",string_sample.len());
    
 
 }
