b9748b9d9f700c1634233b1e577f4b379ef642ca
manimovassagh <mani.mgh@gmail.com>
2022-01-02 00:05:59 +0100

    223

--- src/touples.rs
+++ src/touples.rs
@@ -3,5 +3,6 @@ let persons: (&str, &str, &mut i32) =("Mehdi", "Rahmaniyan", &mut 33);
     println!("My name is {} and my family is {} and i am {} years old",persons.0,persons.1,persons.2);
     println!("Tuple is Printing");
     println!("Tuple is Printing");
+    println!("Tuple is Printing");
 
 }
