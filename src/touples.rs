955535e5ed44c526eaf3465d5c7febb0095a699b
manimovassagh <mani.mgh@gmail.com>
2022-01-02 01:41:30 +0100

    22

--- src/touples.rs
+++ src/touples.rs
@@ -2,7 +2,5 @@ pub fn run(){
 let persons: (&str, &str, &mut i32) =("Mehdi", "Rahmaniyan", &mut 33);
     println!("My name is {} and my family is {} and i am {} years old",persons.0,persons.1,persons.2);
     println!("Tuple is Printing");
-    println!("Tuple is Printing");
-    println!("Tuple is Printing");
-
+  
 }
