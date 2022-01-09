35f6809fe39a6df1113f21d92ada5d0a97d57afa
manimovassagh <mani.mgh@gmail.com>
2022-01-09 01:09:02 +0100

    11

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,7 +1,7 @@
 pub fn run() {
     let mut numbers = [1, 2, 3, 4, 5];
-    numbers[2]:f32=44.5;
-    numbers[2]:f64=44.203942032;
+    numbers[2]=44.5;
+    numbers[2]=44.203942032;
     numbers[2]:f64=44.203942032;
 
 
--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 pub fn tester(){
   println!("Happy new year! 2022");
   println!("Happy new year! 2022");
+
 }
