52b6370b00dbbeaf3657053a000018faee869e0d
manimovassagh <mani.mgh@gmail.com>
2022-01-11 00:36:40 +0100

    11

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,9 +1,5 @@
 pub fn run() {
-    let mut numbers = [1, 2, 3, 4, 5];
-    numbers[2]=44.5;
-    numbers[2]=44.203942032;
-    numbers[2]:f64=44.203942032;
 
+ 
 
-    println!("{:?}", numbers);
 }
