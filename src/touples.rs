579fa83ed6fea5da01e75845decaeba65c9c6891
manimovassagh <mani.mgh@gmail.com>
2022-01-13 23:44:56 +0100

    added

--- src/touples.rs
+++ src/touples.rs
@@ -1,5 +1,6 @@
 pub fn run() {
     let persons: (&str, &str, &mut i32) = ("Mehdi", "Rahmaniyan", &mut 33);
+    let _family_collection: (&str, &str, &mut i32) = ("Rahmanian", "Movassagh", &mut 33);
     println!(
         "My name is {} and my family is {} and i am {} years old",
         persons.0, persons.1, persons.2
